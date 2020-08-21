f68ab5bdbb5393a771be288f70b84038afc8dc0e
NamarG <namar.ghani786@gmail.com>
2020-08-21 20:20:08 +0100

    Update db_config.php

--- Database/db_config.php
+++ Database/db_config.php
@@ -1,8 +1,16 @@
 <?php
-    $host = "localhost";
-    $db = "data_db";
-    $user = "root";
-    $pass = "";
+    // development connection
+    // $host = "localhost";
+    // $db = "data_db";
+    // $user = "root";
+    // $pass = "";
+    // $charset = "utf8mb4";
+
+    // remote database connection
+    $host = "remotemysql.com";
+    $db = "vkoSa8vQBz";
+    $user = "vkoSa8vQBz";
+    $pass = "YkSzLKg6Yg";
     $charset = "utf8mb4";
 
     $dsn = "mysql:host=$host;dbname=$db;charset=$charset"; 
