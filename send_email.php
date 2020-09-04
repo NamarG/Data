d6143f19f5d72d77c88724d3e84b965ab8dad03d
NamarG <namar.ghani786@gmail.com>
2020-09-04 14:24:31 +0100

    fixed error with addcontent function
    error in add content function was causing no emails to be send fixed by adding the content type param

--- send_email.php
+++ send_email.php
@@ -4,12 +4,12 @@ require_once "vendor/autoload.php";
     class SendEmail{
 
         public static function Sendmail($to, $subject, $content){
-            $key = "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";
+            $key = "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";
             $email = new \SendGrid\Mail\Mail();
             $email->setFrom("namaronpc@gmail.com","Namar Ghani");
             $email->setSubject($subject);
             $email->addTo($to);
-            $email->addContent($content);
+            $email->addContent("text/plain", $content);
             
             $sendgrid = new \SendGrid($key);
 
